b92b81ceaf9c10f47046cba896fc98658e6483ec
diegorima98 <drincon2@miners.utep.edu>
2019-11-06 00:17:42 -0700

    added general layout, headers and buttons

---
+++ main/java/edu/utep/cs/cs4330/mypricewatcher/EditItemDialog.java
@@ -0,0 +1,14 @@
+package edu.utep.cs.cs4330.mypricewatcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditItemDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item_dialog);
+    }
+}
